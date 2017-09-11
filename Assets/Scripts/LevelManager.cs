650282b7f31121c0821af9117ffaff841fbffdea
Beatriz <beatriz.zallo@gmail.com>
2017-09-11 17:23:35 +0200

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(int number){
+		Debug.Log ("New Level load: " + number);
+        SceneManager.LoadScene(number);
+    }
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
